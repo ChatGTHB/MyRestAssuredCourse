8f62d7e0477654545fe4a637875520273c1c5d4a
ChatGTHB <cihat.kose@hotmail.com>
2023-04-26 19:36:08 +0300

    task01 and task03 completed

--- src/test/java/Tasks.java
+++ src/test/java/Tasks.java
@@ -1,9 +1,35 @@
 import io.restassured.http.ContentType;
+import model.ToDo;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Tasks {
+
+    /**
+     * Task 1
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * Converting Into POJO
+     */
+
+    @Test
+    public void task01() {
+        ToDo todo =
+                given()
+                        .when()
+                        .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().body().as(ToDo.class);
+        System.out.println("todo = " + todo);
+        System.out.println("todo.getTitle() = " + todo.getTitle());
+    }
+
     /**
      * Task 2
      * create a request to https://httpstat.us/203
@@ -12,14 +38,35 @@ public class Tasks {
      */
 
     @Test
-    public void task2() {
+    public void task02() {
         given()
                 .when()
-                .get("https://httpstat.us/203")
+                // .get("https://httpstat.us/203")
 
                 .then()
                 .log().all()
                 .statusCode(203)
                 .contentType(ContentType.TEXT);
+    }
+
+    /**
+     * Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+    @Test
+    public void task03() {
+                given()
+                        .when()
+                        .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .contentType(ContentType.JSON)
+                        .body("title",equalTo("quis ut nam facilis et officia qui"));
     }
 }
